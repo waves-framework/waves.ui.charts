36d0535a8fe652e3b03b53e81bebdfa279983567
ambertape <khek@ambertape.ru>
2022-11-25 13:12:20 +0300

    Binance candle loading.

--- sources/Waves.UI.Avalonia.Charts.Showcase/ViewModels/MainViewModel.cs
+++ sources/Waves.UI.Avalonia.Charts.Showcase/ViewModels/MainViewModel.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using Avalonia.Threading;
+using Binance.Net.Clients;
+using Binance.Net.Enums;
+using Binance.Net.Interfaces;
 using ReactiveUI.Fody.Helpers;
 using Waves.UI.Charts.Drawing.Primitives;
 using Waves.UI.Charts.Series;
@@ -19,8 +24,39 @@ public class MainViewModel : ViewModelBase
     /// </summary>
     public MainViewModel()
     {
+        SelectedSymbol = "BTCUSDT";
         InitializeChart();
     }
+
+    /// <summary>
+    /// Get or sets symbol.
+    /// </summary>
+    [Reactive]
+    public string SelectedSymbol { get; set; }
+
+    /// <summary>
+    /// Gets or sets X Min.
+    /// </summary>
+    [Reactive]
+    public double XMin { get; set; }
+
+    /// <summary>
+    /// Gets or sets X Max.
+    /// </summary>
+    [Reactive]
+    public double XMax { get; set; }
+
+    /// <summary>
+    /// Gets or sets Y Min.
+    /// </summary>
+    [Reactive]
+    public double YMin { get; set; }
+
+    /// <summary>
+    /// Gets or sets Y Max.
+    /// </summary>
+    [Reactive]
+    public double YMax { get; set; }
 
     /// <summary>
     /// Gets or sets series.
@@ -31,18 +67,26 @@ public class MainViewModel : ViewModelBase
     /// <summary>
     /// Initializes chart.
     /// </summary>
-    private void InitializeChart()
+    private async void InitializeChart()
     {
         Series = new ObservableCollection<IWavesPointSeries>();
 
-        var length = 10000;
+        var candles = (await GetCandles()).ToList();
+        var length = candles.Count;
+
         var x = new double[length];
         var y = new double[length];
+
         for (var i = 0; i < length; i++)
         {
             x[i] = i / (double)length;
-            y[i] = 0.8 * Math.Sin(i / 1000.0d);
+            y[i] = Convert.ToDouble(candles[i].ClosePrice);
         }
+
+        XMin = x.Min();
+        XMax = x.Max();
+        YMin = y.Min();
+        YMax = y.Max();
 
         var series = new WavesPointSeries(x, y)
         {
@@ -52,33 +96,54 @@ public class MainViewModel : ViewModelBase
         Series.Add(series);
 
         var phase = 0d;
-        var task = new Task(async () =>
+        //// var task = new Task(async () =>
+        //// {
+        ////     await Task.Delay(1000);
+        ////
+        ////     do
+        ////     {
+        ////         var random = new Random();
+        ////         var x = new double[length];
+        ////         var y = new double[length];
+        ////         phase += 0.01;
+        ////         for (var i = 0; i < length; i++)
+        ////         {
+        ////             var randomValue = random.NextDouble() / 10;
+        ////             x[i] = i / (double)length;
+        ////             y[i] = randomValue + 0.8 * Math.Sin(i / 1000.0d + phase);
+        ////         }
+        ////
+        ////         if (phase > 2 * Math.PI)
+        ////         {
+        ////             phase = 0;
+        ////         }
+        ////
+        ////         await Dispatcher.UIThread.InvokeAsync(() => { series?.Update(x, y); });
+        ////         await Task.Delay(33);
+        ////     }
+        ////     while (true);
+        //// });
+        //// task.Start();
+    }
+
+    private async Task<IEnumerable<IBinanceKline>> GetCandles()
+    {
+        using (var client = new BinanceClient())
         {
-            await Task.Delay(1000);
+            var result = await client.SpotApi.ExchangeData.GetKlinesAsync(
+                SelectedSymbol,
+                KlineInterval.FiveMinutes,
+                DateTime.Now.AddDays(-1),
+                DateTime.Now);
 
-            do
+            if (result.Success)
             {
-                var random = new Random();
-                var x = new double[length];
-                var y = new double[length];
-                phase += 0.01;
-                for (var i = 0; i < length; i++)
-                {
-                    var randomValue = random.NextDouble() / 10;
-                    x[i] = i / (double)length;
-                    y[i] = randomValue + 0.8 * Math.Sin(i / 1000.0d + phase);
-                }
-
-                if (phase > 2 * Math.PI)
-                {
-                    phase = 0;
-                }
-
-                await Dispatcher.UIThread.InvokeAsync(() => { series?.Update(x, y); });
-                await Task.Delay(33);
+                return result.Data;
             }
-            while (true);
-        });
-        task.Start();
+            else
+            {
+                throw new Exception($"Error requesting data: {result.Error.Message}");
+            }
+        }
     }
 }
